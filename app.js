a2f57c644cf471b5d3b06e734a835dc7db2ce52e
IvanKapitula <ivan.kapitula@yandex.ru>
2023-08-03 17:23:51 +0300

    feat: add some code and testing programm

---
+++ app.js
@@ -0,0 +1,11 @@
+const sister = document.querySelector(".sister");
+
+function changeColor() {
+  sister.style.color = "red";
+}
+
+function changeSize() {
+  sister.style.fontSize = "130px";
+}
+changeColor();
+changeSize();
